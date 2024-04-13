850ad00cd920563c67a9276ff09fcf3a6e9737e2
alena <aasazy190@gmail.com>
2024-04-13 21:58:28 +0300

    Add task_1

--- task_2/test_get_screen.py
+++ task_2/test_get_screen.py
@@ -1,22 +1,69 @@
+import pytest
 from playwright.sync_api import sync_playwright, Playwright
 from playwright.sync_api import expect
-from datetime import datetime
+import os
 
 
-def run(playwright: Playwright):
-    browser = playwright.chromium.launch(channel="chrome", headless=False)
-    context = browser.new_context(ignore_https_errors=True)
-    page = context.new_page()
-    page.goto("https://www.avito.ru/avito-care/eco-impact")
-    # import time
-    # time.sleep(5000000)
-    # page.locator("html").press("PageDown")
-    element = page.locator("xpath=//html/body/div[1]/div/div[3]/div/div/div/div/div[3]/div/div[2]/div[4]")
-    expect(element).to_be_visible()
-    element.screenshot(path=f"./screenshot_test_1.png")
-    # Test the background page as you would any other page.
-    browser.close()
 
-def test_1():
-    with sync_playwright() as playwright:
-        run(playwright)
+@pytest.fixture()
+def page():
+    """Браузер и контекст"""
+    playwright = sync_playwright().start()
+    browser = playwright.chromium.launch(headless=True,slow_mo=1)
+    context = browser.new_context(viewport={"height":1920,"width":1080},locale="ru-RU")
+    page_data = context.new_page()
+    yield page_data
+
+
+class TestAvitoScreenshots:
+
+
+    @pytest.fixture(autouse=True)
+    def setup_test(self,page):
+        """"""
+        self.base_dir = os.path.dirname(os.path.realpath(__file__))
+        self.check_dir()
+        self.url = "https://www.avito.ru/avito-care/eco-impact"
+        self.locator_1 = "xpath=//html/body/div[1]/div/div[3]/div/div/div/div/div[3]/div/div[2]/div[4]"
+        self.locator_2 = "xpath=//html/body/div[1]/div/div[3]/div/div/div/div/div[3]/div/div[2]/div[2]"
+        self.locator_3 = "xpath=//html/body/div[1]/div/div[3]/div/div/div/div/div[3]/div/div[2]/div[6]"
+        self.page = page
+        
+
+
+    def check_dir(self):
+        """Проверка наличия директории output, создание директории при ее отсутствии"""
+        if not os.path.exists(os.path.join(self.base_dir,"output")):
+            os.mkdir(os.path.join(self.base_dir,"output"))
+
+    
+   
+    def test(self):
+        """Обнаружение локатора на странице, скриншот элемента и сохранение скриншота в директории output"""
+        self.page.goto(self.url)
+        element_1 = self.page.locator(self.locator_1)
+        expect(element_1).to_be_visible()
+        element_1.screenshot(path=os.path.join(self.base_dir,"output",f"screenshot_test_1.png"))
+        element_2=self.page.locator(self.locator_2)
+        expect(element_2).to_be_visible()
+        element_2.screenshot(path=os.path.join(self.base_dir,"output",f"screenshot_test_2.png"))
+        element_3 = self.page.locator(self.locator_3)
+        expect(element_3).to_be_visible()
+        element_3.screenshot(path=os.path.join(self.base_dir,"output",f"screenshot_test_3.png"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
